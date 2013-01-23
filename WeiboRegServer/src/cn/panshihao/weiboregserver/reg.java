e650a5f16d24ff702291f1b4093c68e626853873
shihao <shihao@panshihao>
2013-01-23 20:04:18 +0800

    test

---
+++ WeiboRegServer/src/cn/panshihao/weiboregserver/reg.java
@@ -0,0 +1,18 @@
+package cn.panshihao.weiboregserver;
+
+import sun.net.www.http.HttpClient;
+
+public class reg {
+
+	
+	public static void main(String[] args) {
+		
+		
+		
+//		HttpClient httpClient = new HttpClient(arg0, arg1, arg2)
+		
+		
+	}
+	
+	
+}
