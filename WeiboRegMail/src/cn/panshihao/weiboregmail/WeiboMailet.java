164a650cee705bca62bcf8d04604a51b22782956
shihao <shihao@panshihao>
2013-01-23 23:03:51 +0800

    环境调试完成

---
+++ WeiboRegMail/src/cn/panshihao/weiboregmail/WeiboMailet.java
@@ -0,0 +1,16 @@
+package cn.panshihao.weiboregmail;
+
+import javax.mail.MessagingException;
+
+import org.apache.mailet.Mail;
+import org.apache.mailet.base.GenericMailet;
+
+public class WeiboMailet extends GenericMailet{
+
+	@Override
+	public void service(Mail arg0) throws MessagingException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
