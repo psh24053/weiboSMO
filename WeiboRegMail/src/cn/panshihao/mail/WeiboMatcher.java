9c8178302d9cdc042e69e7988f039b12cc660792
psh24053 <psh24053@yahoo.cn>
2013-01-30 00:12:16 +0800

    weibomatcher

--- WeiboRegMail/src/cn/panshihao/mail/WeiboMatcher.java
+++ WeiboRegMail/src/cn/panshihao/mail/WeiboMatcher.java
@@ -12,6 +12,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 
 import javax.mail.BodyPart;
@@ -19,21 +20,28 @@ import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
 
+import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpVersion;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.CookieStore;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.params.ClientPNames;
 import org.apache.http.client.params.CookiePolicy;
 import org.apache.http.conn.params.ConnRoutePNames;
+import org.apache.http.cookie.Cookie;
 import org.apache.http.cookie.params.CookieSpecPNames;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.conn.PoolingClientConnectionManager;
+import org.apache.http.impl.cookie.BasicClientCookie;
 import org.apache.http.message.BasicHeader;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.params.CoreConnectionPNames;
 import org.apache.http.params.CoreProtocolPNames;
 import org.apache.james.transport.matchers.GenericRegexMatcher;
@@ -76,8 +84,10 @@ public class WeiboMatcher extends GenericMatcher {
 				
 				MailAddress recipient = (MailAddress) mail.getRecipients().toArray()[0];
 				
+				System.out.println(url);
+				
 				connectURL(url);
-				updateDB(recipient.getUser()+"@"+DOMAIN);
+//				updateDB(recipient.getUser()+"@"+DOMAIN);
 			}
 			
 			
@@ -95,6 +105,24 @@ public class WeiboMatcher extends GenericMatcher {
 	 */
 	public void connectURL(String url){
 		
+		String[] params = url.split("?")[1].split("&");
+		
+		System.out.println(params.length);
+		
+		List<NameValuePair> formParams = new ArrayList<NameValuePair>();
+		
+		for(int i = 0 ; i < params.length ; i ++){
+			String[] item = params[i].split("=");
+			
+			String key = item[0];
+			String value = item.length > 1 ? item[1] : "";
+			
+			formParams.add(new BasicNameValuePair(key, value));
+		}
+		
+		
+		
+		
 		/**
 		 * 创建多线程连接器
 		 */
@@ -105,46 +133,39 @@ public class WeiboMatcher extends GenericMatcher {
 		HttpClient httpClient = new DefaultHttpClient(connectionManager);
 		
 		//伪装成Firefox 5, 
-		httpClient.getParams().setParameter(CoreProtocolPNames.PROTOCOL_VERSION, HttpVersion.HTTP_1_1); httpClient.getParams().setParameter(ClientPNames.COOKIE_POLICY, CookiePolicy.BROWSER_COMPATIBILITY); // 一定要有，否则会生成多个Cookie header送给web server 
-		httpClient.getParams().setParameter(CookieSpecPNames.SINGLE_COOKIE_HEADER, true); //
+		httpClient.getParams().setParameter(CoreProtocolPNames.PROTOCOL_VERSION, HttpVersion.HTTP_1_1); 
+		httpClient.getParams().setParameter(ClientPNames.COOKIE_POLICY, CookiePolicy.IGNORE_COOKIES); //
 		httpClient.getParams().setParameter(CoreProtocolPNames.HTTP_CONTENT_CHARSET,"UTF-8"); //这个是和目标网站的编码有关；
 		httpClient.getParams().setParameter(CoreProtocolPNames.HTTP_ELEMENT_CHARSET,"UTF-8"); 
-		List<BasicHeader> headerList = new ArrayList<BasicHeader>(); 
-		headerList.add(new BasicHeader("Accept", "*/*")); 
-		headerList.add(new BasicHeader("Accept-Charset", "GBK,utf-8;q=0.7,*;q=0.3"));
-		headerList.add(new BasicHeader("Accept-Language", "zh-CN,zh;q=0.8"));
-		headerList.add(new BasicHeader("Cache-Control", "no-cache")); 
-		headerList.add(new BasicHeader("Connection", "keep-alive"));
-		headerList.add(new BasicHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8"));
-		headerList.add(new BasicHeader("X-Requested-With", "XMLHttpRequest"));
-		headerList.add(new BasicHeader("Host", "www.weibo.com"));
-		headerList.add(new BasicHeader("Origin", "http://www.weibo.com"));
-		headerList.add(new BasicHeader("Referer", "http://www.weibo.com/signup/mobile.php"));
-		httpClient.getParams().setParameter(ClientPNames.DEFAULT_HEADERS, headerList);
 		httpClient.getParams().setParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, new Integer(30000)); 
 		httpClient.getParams().setParameter(CoreConnectionPNames.SO_TIMEOUT,  new Integer(30000) ); 
 		httpClient.getParams().setParameter(ClientPNames.CONN_MANAGER_TIMEOUT, new Long(30000)); // second;
 		httpClient.getParams().setParameter(CoreProtocolPNames.USER_AGENT, "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:17.0) Gecko/17.0 Firefox/17.0");
 		
-        
+		
         // 请求注册界面，获取表单必须参数
-        HttpGet httpget = new HttpGet(url);
+        HttpPost httpPost = new HttpPost("http://weibo.com/signup/v5/active");
+        try {
+			httpPost.setEntity(new UrlEncodedFormEntity(formParams, "UTF-8"));
+		} catch (UnsupportedEncodingException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		HttpResponse httpResponse = null;
 		try {
-			httpResponse = httpClient.execute(httpget);
+			httpResponse = httpClient.execute(httpPost);
 		} catch (ClientProtocolException e) {
 //			Log.log.error(e.getMessage(), e);
 		} catch (IOException e) {
 //			Log.log.error(e.getMessage(), e);
 		}
-		HttpEntity httpEntity = httpResponse.getEntity();
 		
 		
-		httpResponse.getHeaders("Location");
+		
+		
 		
 		try {
-			String html = HtmlTools.getHtml(httpEntity);
-			
+			System.out.println(toLocation(httpClient, httpResponse));
 		} catch (UnsupportedEncodingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -156,20 +177,79 @@ public class WeiboMatcher extends GenericMatcher {
 			e.printStackTrace();
 		}
 		
-		httpget = new HttpGet("http://www.weibo.com");
-		try {
-			httpClient.execute(httpget);
-		} catch (ClientProtocolException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		
 		System.out.println("已点击连接 "+url);
 		
 	}
 	
+	/**
+	 * 手动location，并且一路上手动设置cookie
+	 * @param httpClient
+	 * @param httpResponse
+	 * @return
+	 * @throws IOException 
+	 * @throws IllegalStateException 
+	 * @throws UnsupportedEncodingException 
+	 */
+	public String toLocation(HttpClient httpClient, HttpResponse httpResponse) throws UnsupportedEncodingException, IllegalStateException, IOException{
+		Header[] headers = httpResponse.getAllHeaders();
+		
+		System.out.println(HtmlTools.getHtmlByBr(httpResponse.getEntity()));
+		
+		String location = null;
+		
+		for(int i = 0 ; i < headers.length ; i ++){
+			String name = headers[i].getName();
+			String value = headers[i].getValue();
+			
+			System.out.println("name: "+name+" ,value: "+value);
+			
+			if(name.equals("Location")){
+				location = value;
+			}
+			if(name.equals("Set-Cookie")){
+				CookieStore cookieStore = ((DefaultHttpClient)httpClient).getCookieStore();
+				
+				String[] cookies = value.split(";");
+				
+				BasicClientCookie cookieClient = null;
+				
+				for(int j = 0 ; j < cookies.length ; j ++){
+					String[] cookie = cookies[j].trim().split("=");
+					if(j == 0){
+						cookieClient = new BasicClientCookie(cookie[0], cookie[1]);
+					}
+					
+					if(cookie[0].equals("expires")){
+						cookieClient.setExpiryDate(new Date(cookie[1]));
+					}
+					if(cookie[0].equals("path")){
+						cookieClient.setPath(cookie[1]);
+					}
+					if(cookie[0].equals("domain")){
+						cookieClient.setDomain(cookie[1]);
+					}
+					
+				}
+				if(cookieClient != null){
+					cookieStore.addCookie(cookieClient);
+					((DefaultHttpClient)httpClient).setCookieStore(cookieStore);
+				}
+			}
+			
+		}
+		
+		if(location == null){
+			
+			return HtmlTools.getHtmlByBr(httpResponse.getEntity());
+			
+		}else{
+			toLocation(httpClient, httpClient.execute(new HttpGet("http://weibo.com"+location)));
+		}
+		
+		return null;
+		
+	}
 	
 	
 	/**
