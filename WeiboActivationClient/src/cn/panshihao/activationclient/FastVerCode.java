7e73774a1fce7e1e235309ccd91d7355df90d725
Panshihao <psh24053@Yahoo.cn>
2013-02-05 22:46:12 +0800

    LoginService

---
+++ WeiboActivationClient/src/cn/panshihao/activationclient/FastVerCode.java
@@ -0,0 +1,20 @@
+package cn.panshihao.activationclient;
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.WString;
+
+public interface FastVerCode extends Library {
+	
+	FastVerCode	INSTANCE = (FastVerCode) Native.loadLibrary("FastVerCode", FastVerCode.class);
+
+	public String GetUserInfo(String UserName, String passWord);
+
+	public String RecByte(byte[] imgByte, int len, String username, String password);
+
+	public String RecYZM(String path, String UserName, String passWord);
+
+	public void ReportError(WString UserName, WString passWord);
+
+	public int Reglz(String userName, String passWord, String email, String qq, String dlId, String dlAccount);
+}
